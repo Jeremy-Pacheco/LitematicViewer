89391f2375d66254c3f936f985a77dbabab492be
Jeremy-Pacheco <pachecoonajeremypaul@gmail.com>
2026-03-19 13:17:44 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LitematicViewer/',
+  base: '/',
   plugins: [react()],
   define: {
     global: 'globalThis',
