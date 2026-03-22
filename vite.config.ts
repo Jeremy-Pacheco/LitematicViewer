8f909b6c09b0632de76563d4d18b808f48915306
Jeremy-Pacheco <pachecoonajeremypaul@gmail.com>
2026-03-22 20:42:25 +0000

    add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer/',
+  // https://vite.dev/config/
+  export default defineConfig({
+    base: '/LitematicViewer/',
+    plugins: [react()],
+    define: {
+      global: 'globalThis',
+      native: 'globalThis',
     },
-  },
-})
+    resolve: {
+      alias: {
+        buffer: 'buffer/',
+      },
+    },
+    build: {
+      commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    }
+  })
